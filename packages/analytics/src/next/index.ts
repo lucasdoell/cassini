ae8d25a0cd1ef8d2ed6e4788a34d1354e294c597
Lucas Doell <lucas@lux.dev>
2025-01-11 19:20:57 -0500

    Update exported functions from index.ts

--- packages/analytics/src/next/index.ts
+++ packages/analytics/src/next/index.ts
@@ -1,3 +1,3 @@
-export { Analytics } from "./analytics";
-export type { CassiniAnalyticsProps } from "./analytics";
-export { analyticsMiddleware } from "./middleware";
+"use client";
+
+export * from "./analytics";
