7d03fc523f7c6178ed500271995ce5bc1b53f000
Lucas Doell <lucas@lux.dev>
2025-01-11 19:50:10 -0500

    Update middleware import

--- examples/todo-app/middleware.ts
+++ examples/todo-app/middleware.ts
@@ -1,4 +1,4 @@
-import { analyticsMiddleware } from "@cassini/analytics/dist/next/server.js";
+import { analyticsMiddleware } from "@cassini/analytics/next/server";
 import { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
