e21b9c913c765266fe11b4a01a6d0559824501f3
Lucas Doell <lucas@lux.dev>
2025-01-11 19:20:26 -0500

    Update analytics endpoint URL

--- packages/analytics/src/config.ts
+++ packages/analytics/src/config.ts
@@ -1,7 +1,7 @@
 import { AnalyticsConfig } from "./types";
 
 export const DEFAULT_CONFIG: Required<AnalyticsConfig> = {
-  endpoint: "http://localhost:8080/",
+  endpoint: "http://localhost:3001/analytics",
   apiKey: "", // Will be automatically populated from env vars
   debug: false,
   disabled: false,
