749fcfcd2c72c8d2ce1a6d4f41240247e24f33de
Lucas Doell <lucas@lux.dev>
2025-01-11 19:32:45 -0500

    Add analytics middleware

---
+++ examples/todo-app/middleware.ts
@@ -0,0 +1,19 @@
+import { analyticsMiddleware } from "@cassini/analytics/dist/next/server.js";
+import { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  return analyticsMiddleware(request);
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - public folder files
+     */
+    "/((?!_next/static|_next/image|favicon.ico|public/).*)",
+  ],
+};
